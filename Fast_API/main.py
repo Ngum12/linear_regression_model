cb5a501cf079065222c60ccf90b4a82ccb542c2d
Ngum Dieudonne <127442228+Ngum12@users.noreply.github.com>
2024-11-23 07:30:08 +0200

    Update main.py

--- Fast_API/main.py
+++ Fast_API/main.py
@@ -61,4 +61,4 @@ async def predict(input_data: PredictionInput):
 # Run the app
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
