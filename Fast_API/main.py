e46333819edeaaf9cf32d44a0749e84e7311d3c3
Ngum Dieudonne <127442228+Ngum12@users.noreply.github.com>
2024-11-24 06:06:42 +0200

    Update main.py

--- Fast_API/main.py
+++ Fast_API/main.py
@@ -61,4 +61,5 @@ async def predict(input_data: PredictionInput):
 # Run the app
 if __name__ == "__main__":
     import uvicorn
+    port = int(os.getenv("PORT", 8000))  # Default to 8000 if PORT is not set
     uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
